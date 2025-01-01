229fec9d0eee7c8facbae03c6047ad7c4c286b5c
Junior-Wick <francisco.neto22_@outlook.com>
2025-01-01 05:35:07 -0300

    criacao da classe Product, atributos e funcoes no arquivo product.py

--- src/_product.py
+++ src/_product.py
@@ -7,3 +7,28 @@
 - remover_produto()
 - listar_produtos()
 """
+
+
+class Product:
+
+    counter_id = 0
+
+    def __init__(self, name: str, price: int, quantity: int):
+        self.__id = Product.counter_id + 1
+        self.__name = name
+        self.__price = price
+        self.__quantity = quantity
+        Product.counter_id = self.__id
+
+
+    def add_product(self):
+        pass
+
+    def edit_product(self):
+        pass
+
+    def remove_product(self):
+        pass
+
+    def list_product(self):
+        pass
