91ce05760dc09c12d4179cae3f1180607815c901
Junior-Wick <francisco.neto22_@outlook.com>
2025-01-05 20:00:01 -0300

    Ajustes realizados nos métodos da classe Product (_product.py)

--- src/_product.py
+++ src/_product.py
@@ -1,6 +1,7 @@
 from _databases import DataBases
-from datetime import datetime
 from sqlite3 import DatabaseError
+from _functions import set_time
+from typing import Union
 
 """
 1. Produto
@@ -16,19 +17,20 @@ db = DataBases()
 
 class Product:
 
+    cursor = db.get_cursor
 
     def __init__(self, name: str, price: int, quantity: int):
         self.__name = name
         self.__price = price
         self.__quantity = quantity
 
-
+    # Método está OK
     def add_product(self):
         if self.__name is None or self.__price is None or self.__quantity is None:
             raise ValueError("Dados do produto invalidos!")
         
-        cursor = db.get_cursor
-        cursor.execute(
+        creat_at = set_time()
+        Product.cursor.execute(
             """
             INSERT into Product (ProductName, ProductPrice, ProductQuantity, CreateAT, Alteration) VALUES
             (?, ?, ?, ?, ?);
@@ -36,102 +38,97 @@ class Product:
                 self.__name, 
                 self.__price, 
                 self.__quantity,
-                datetime.now().strftime("%Y/%m/%d - %H:%M:%S"),
-                datetime.now().strftime("%Y/%m/%d - %H:%M:%S"))
+                creat_at,
+                creat_at)
         )
 
         db.commit_changes()
-        db.close_connetion()
-        
+
+        return f"Produto '{self.__name}' adicionado com sucesso!"
+
+
+    # Método está OK    
     @staticmethod
-    def edit_product():
-        Product.list_product()
-        print()
-        db = DataBases()
+    def edit_product(product_id: int, new_name: str = None, new_price: int = None, new_quantity: int = None) -> Union [str, int]:
+        """
+        Método responsável por realizar as alterações dentro dos cadastros dos produtos.
 
-        def change_name():
-            try:
-                product_id = int(input("Digite o ID do produto que deseja alterar o nome: "))
-                new_name = str(input("Digite o novo nome do produto: "))
-                if not new_name:
-                    print("O nome do produto não pode ser vazio.")
-                    return
-                cursor = db.get_cursor
-                cursor.execute("UPDATE Product SET ProductName = ? WHERE ProductID = ?", (new_name, product_id))
-                db.commit_changes()
-                print("Nome do produto atualizado com sucesso!")
+        :param produt_id: ID do produto cadastrado no banco
+        :param new_name: Parâmetro Opcional do método caso queira alterar o nome do produto
+        :param new_price: Parâmetro Opcional do método caso queira alterar o preço do produto
+        :param new_quantity: Parâmetro Opcional do método caso queira alterar a quantidade do produto em estoque
+        :return: Retorna uma mensagem informando que a alteração foi realizada com sucesso
+        """
 
-            except DatabaseError as Er:
-                print(f"Erro no banco de dados {Er}")
-
-            except Exception as Er:
-                print(f"Erro ao atualizar o nome: {Er}")
-
-        def change_price():
-            try:
-                product_id = int(input("Digite o ID do produto que deseja alterar o preço: "))
-                new_price = int(input("Digite o novo preço do produto: "))
-                if not new_price:
-                    print("O preço do produto não pode ser vazio!")
-                    return
-                cursor = db.get_cursor
-                cursor.execute("UPDATE Product SET ProductPrice = ? WHERE ProductID = ?", (new_price, product_id))
-                db.commit_changes()
-                print("Preço do produto atualizado com sucesso!")
-
-            except DatabaseError as Er:
-                print(f"Erro no banco de dados {Er}")
-
-            except Exception as Er:
-                print(f"Erro ao atualizar o preço: {Er}")
-
-        def change_quantity():
-            try:
-                product_id = int(input("Digite o ID do produto que deseja alterar a quantidade: "))
-                new_quantity = int(input("Digite a nova quantidade do produto: "))
-                if not new_quantity:
-                    print("A quantidade não pode ser vazio!")
-                    return
-                cursor = db.get_cursor
-                cursor.execute("UPDATE Product SET ProductQuantity = ? WHERE ProductID = ?", (new_quantity, product_id))
-                db.commit_changes()
-                print("Quantidade do produto atualizada com sucesso!")
-
-            except DatabaseError as Er:
-                print(f"Erro no banco de dados {Er}")
-
-            except Exception as Er:
-                print(f"Erro ao atualizar a quantidade: {Er}")    
+        if product_id <= 0:
+            raise ValueError("ID de produto inválido")
         
-        # Menu de ações para edição dos produtos (teste)
-        while True:
-            choice = input("=== Menu === \n1) Nome \n2) Preço \n3) Quantidade \n4) Sair \nEscolha a opção que deseja alterar: ")
-            if choice == "1":
-                change_name()
-            elif choice == "2":
-                change_price()
-            elif choice == "3":
-                change_quantity()
-            elif choice == "4":
-                break
-            else:
-                print("Escolha inválida! Tente novamente.")
+        updates = {}
+        if new_name:
+            updates["ProductName"] = new_name
+        if new_price:
+            updates["ProductPrice"] = new_price
+        if new_quantity:
+            updates["ProductQuantity"] = new_quantity
 
-    def remove_product(self):
-        pass
+        updates["Alteration"] = set_time()
+
+        if not updates:
+            raise ValueError("Nenhum campo para atualizar foi fornecido")
+
+        set_clause = ", ".join(f"{key} = ?" for key in updates.keys())
+        values = list(updates.values()) + [product_id]
+
+        query = f"UPDATE Product SET {set_clause} WHERE ProductID = ?;"
+        Product.cursor.execute(query, values)
+
+        db.commit_changes()
+
+        return 'Alteração realizada com sucesso!'
+
+
+    # Método está OK
+    @staticmethod
+    def delete_product(product_id: int) -> int:
+        
+        if product_id <= 0:
+            raise ValueError("ID do produto inválido!")
+        
+        try:
+            product = Product.cursor.execute(
+                """
+                SELECT ProductName from Product WHERE ProductID = ?
+                """, (
+                    product_id,
+                )
+            ).fetchone()[0]  
+        # Essa exceção irá tratar casos em que o ID não existir no banco
+        except TypeError:
+            return f"Erro: O ID de produto '{product_id}' não existe!"
+
+        Product.cursor.execute(
+            """
+            DELETE FROM Product WHERE ProductID = ?
+            """, (
+                product_id,
+            )
+        )
+
+        db.commit_changes()
+
+        return f"Produto '{product}' deletado com sucesso!"
+
     
+    # Método está OK
     @staticmethod
     def list_product():
-        cursor = db.get_cursor
-        cursor.execute("SELECT ProductID, ProductName, ProductPrice, ProductQuantity FROM Product;")
-  
-        result = cursor.fetchall()
-        print(result)
-        for res in result:
-            print(f"ID: {res[0]}, Nome: {res[1]}, Preço: {res[2]}, Quantidade: {res[3]}")
-
-        db.close_connetion()
-
         
+        result = Product.cursor.execute("SELECT ProductID, ProductName, ProductPrice, ProductQuantity FROM Product;").fetchall()
+        
+        return [res for res in result]
+
+       
 if __name__ == "__main__":
     Product.list_product()
+
+print(Product.list_product())
